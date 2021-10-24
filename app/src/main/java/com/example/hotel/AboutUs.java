a43fd80bb6ce3406d7e5ea379775e069ae2956d9
sakspari-PC <gitfreder@gmail.com>
2021-10-24 16:15:13 +0800

    add About Us

--- app/src/main/java/com/example/hotel/AboutUs.java
+++ app/src/main/java/com/example/hotel/AboutUs.java
@@ -1,24 +1,19 @@
-package com.example.hotel.view.home;
+package com.example.hotel;
 
 import android.os.Bundle;
 
-import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.hotel.R;
-import com.example.hotel.databinding.FragmentAboutUsBinding;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FragmentAboutUs#newInstance} factory method to
+ * Use the {@link AboutUs#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FragmentAboutUs extends Fragment {
-    FragmentAboutUsBinding binding;
+public class AboutUs extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,7 +24,7 @@ public class FragmentAboutUs extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FragmentAboutUs() {
+    public AboutUs() {
         // Required empty public constructor
     }
 
@@ -39,11 +34,11 @@ public class FragmentAboutUs extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentAboutUs.
+     * @return A new instance of fragment AboutUs.
      */
     // TODO: Rename and change types and number of parameters
-    public static FragmentAboutUs newInstance(String param1, String param2) {
-        FragmentAboutUs fragment = new FragmentAboutUs();
+    public static AboutUs newInstance(String param1, String param2) {
+        AboutUs fragment = new AboutUs();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,8 +59,6 @@ public class FragmentAboutUs extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_about_us, container, false);
-
-        return binding.getRoot();
+        return inflater.inflate(R.layout.fragment_about_us, container, false);
     }
 }
--- app/src/main/java/com/example/hotel/view/home/HomeFragment.java
+++ app/src/main/java/com/example/hotel/view/home/HomeFragment.java
@@ -93,7 +93,7 @@ public class HomeFragment extends Fragment {
         binding.aboutUs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Navigation.findNavController(binding.getRoot()).navigate(R.id.action_homeFragment_to_AboutUs);
             }
         });
 
